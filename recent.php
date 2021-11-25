b2b8e77c2b17aada1ff14d4409e7dff657f7015a
sheikh ammar <sheikhammar568@gmail.com>
2021-11-25 18:13:44 +0500

    Some Modifiactions After Preloader Added

--- recent.php
+++ recent.php
@@ -1,13 +1,17 @@
 <?php include "header.php"; ?>
 
+
+
+
+
     <section>
-        <br><br><br><br><br><br>
-        <div class="container-fluidmy-5">
+        <div class="container my-5">
             <img src="./images/recent.png" alt="Recent Under Construction" width="100%" height="100%">
         </div>
     </section>
     <div class="text-center my-5">
         <a href="index.php" class="btn btn-success my-3">Back To Home</a>
     </div>
+    <br><br><br>
 
 <?php include "footer.php"; ?>
